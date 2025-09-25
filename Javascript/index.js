951df0e497dd86720a17fe9fa55d2a459aed1f43
isaac1993-io <isaacmogorlama@gmail.com>
2025-09-26 04:42:19 +0530

    javascript

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,6 @@
 
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px";   // Changes text size to 25px
+  x.style.color = "red";       // Changes text color to red
+}
